26a035f75070d35c9d9502156356e10da6e6b209
rcamarano <rogeriocamarano@gmail.com>
2023-05-10 15:03:23 -0300

    iniciando o projeto

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const app = require('./app');
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3001;
 
-app.listen(port, () => console.log('ouvindo porta', port));
+app.listen(port, () => console.log('RC blogs api ouvindo porta', port));
