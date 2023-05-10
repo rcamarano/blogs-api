6a011db9b99fa31c730e83685f87d370bf435937
rcamarano <rogeriocamarano@gmail.com>
2023-05-10 19:11:14 -0300

    req 2 create the model User, passing on local tests

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {*} DataTypes
+ * @returns
+ */
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+      'User',
+      {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        display_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING
+      },
+      {
+        timestamps: false,
+        tableName: 'users',
+      },
+    );
+    // User.associate = (models) => {
+    //   User.hasMany(models.Product, {
+    //     foreignKey: 'userId',
+    //     as: 'products',
+    //   });
+    // };
+    return User;
+  };
+  
