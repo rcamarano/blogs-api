d027153e9055886ca68ee97d66b17f7cbf367a82
rcamarano <rogeriocamarano@gmail.com>
2023-05-10 17:09:21 -0300

    req 01 executing the migrations and seeding the table, fisrt attempt

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
+// const apiRoutes = require('./routes');
 
 // ...
 
 const app = express();
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use(apiRoutes);
 // não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
