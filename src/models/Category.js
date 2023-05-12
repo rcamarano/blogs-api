fce69d24926f28e1bb3ac75ecbf9b7b85075dfd4
rcamarano <rogeriocamarano@gmail.com>
2023-05-12 15:40:07 -0300

    req 07 Category Model created, passing on local tests

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+const CategoryModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define(
+      'Category',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        timestamps: false,
+        tableName: 'categories',
+      },
+    );
+  
+    return Category;
+  };
+  
+  module.exports = CategoryModel;
